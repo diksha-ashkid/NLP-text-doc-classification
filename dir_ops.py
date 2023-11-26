88c55735744d2c39205d6270e1e2e739f5fcd34e
aakashrajaraman <20btrcl069@jainuniversity.ac.in>
2023-11-26 20:07:21 +0530

    creating demo

--- dir_ops.py
+++ dir_ops.py
@@ -3,36 +3,45 @@ import shutil
 import logging
 
 # Setting up logging
-logging.basicConfig(filename='classification.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-# Function to classify and move files
-def classify_document(file_path, class_label):
-    # Define your class labels and corresponding folder paths
-    class_folders = {
-        'News': '/path/to/class1/folder',
-        'Research paper': '/path/to/class2/folder',
-        'Code': '/path/to/class3/folder',
-        'Legal': '/path/to/class4/folder',
-        'Financial Documents': '/path/to/class5/folder',
-        'Medical': '/path/to/class6/folder',
-        # Add more classes and folder paths as needed
-    }
-    
-    # Check if the class label exists
-    if class_label in class_folders:
-        destination_folder = class_folders[class_label]
+#use all this in a class
+class Move:
+    def __init__(self):
+        logging.basicConfig(filename='classification.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+    # Function to classify and move files
+    def classify_document(self, file_path, class_label):
+        # Define your class labels and corresponding folder paths
+        class_folders = {
+            'News': "D:/Backup/Desktop/programs/NLP-text-doc-classification/demo/news",
+            'Research paper': "D:/Backup/Desktop/programs/NLP-text-doc-classification/demo/research",
+            'Code': "D:/Backup/Desktop/programs/NLP-text-doc-classification/demo/code",
+            'Legal': "D:/Backup/Desktop/programs/NLP-text-doc-classification/demo/legal",
+            'Financial Documents': "D:/Backup/Desktop/programs/NLP-text-doc-classification/demo/financial",
+            'Medical': "D:/Backup/Desktop/programs/NLP-text-doc-classification/demo/medical",
+            # Add more classes and folder paths as needed
+        }
         
-        # Move the file to the corresponding folder
-        try:
-            shutil.move(file_path, destination_folder)
-            logging.info(f"Moved file {file_path} to {destination_folder}")
-        except Exception as e:
-            logging.error(f"Error moving file {file_path}: {e}")
-    else:
-        logging.warning(f"Class label {class_label} not found.")
+        # Check if the class label exists
+        if class_label in class_folders:
+            destination_folder = class_folders[class_label]
+            
+            # Move the file to the corresponding folder
+            try:
+                shutil.move(file_path, destination_folder)
+                print("here")
+                logging.info(f"Moved file {file_path} to {destination_folder}")
+            except Exception as e:
+                logging.error(f"Error moving file {file_path}: {e}")
+        else:
+            logging.warning(f"Class label {class_label} not found.")
 
 # Example usage:
-file_to_classify = '/path/to/your/document.txt'
-assigned_class = 'class1'
+file_to_classify = "D:/Backup/Desktop/programs/NLP-text-doc-classification/demo/ex.txt"
+assigned_class = 'News'
 
-classify_document(file_to_classify, assigned_class)
+#classify_document(file_to_classify, assigned_class)
+
+#example 
+ex = Move()
+ex.classify_document(file_to_classify, assigned_class)
